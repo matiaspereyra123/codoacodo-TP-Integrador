041de0c73eb005e56e36b6f3a7a5f4033ee7efe4
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2022-11-06 11:02:24 -0300

    validar positivos y msg alert danger

--- js/ticket.js
+++ js/ticket.js
@@ -1,9 +1,4 @@
 
-  /* 
-    Pedir al usuario nombre y en qué año nació.
-    La máquina procesa y luego lo saluda y
-    le indica cuántos años tiene.
-*/
     let btnResumen;
     let btnBorrar;
     let selectCategoria;
@@ -12,41 +7,69 @@
     let totalSinDescuento;
     let precio=200;
     let totalPagar;
+    let alert;
+    let msgPagar;
 
 totalPagar = document.getElementById("totalPagar");
 btnBorrar=document.getElementById("btn-borrar");
 btnResumen = document.getElementById("btn-resumen");
+alert = document.getElementById("alert");
+msgPagar = document.getElementById("msg-pagar");
 
  function procesar(e) {
     e.preventDefault();
 
 selectCategoria = document.getElementById("selectCategoria").value;
-inputCantidad = document.getElementById("input-cantidad").value;    
+inputCantidad = document.getElementById("input-cantidad").value; 
 
-function descuentoTicket(categoria,cantidad){
-        totalSinDescuento = cantidad*precio;
+
+function calcularValorTicket(cantidad,precio){
+    totalSinDescuento = cantidad*precio;
+    return totalSinDescuento;
+}   
+
+function validarPositivo(cantidad){
+    if(cantidad<=0){
+       alert.classList.remove("alert-primary")
+       alert.classList.add("alert-danger");
+       msgPagar.innerText="";
+       totalPagar.innerText="Debe ingresar un valor positivo";
+    }else{
+        msgPagar.innerText= "Total a Pagar: $";
+        alert.classList.remove("alert-danger");
+        alert.classList.add("alert-primary");
+        totalPagar.innerText = 0;
+        descuentoTicket(selectCategoria); 
+        
+    }
+}
+
+validarPositivo(inputCantidad);
+
+ function descuentoTicket(categoria){
+  
+        totalSinDescuento = calcularValorTicket(inputCantidad,precio);
+
         if(categoria ==1){
             totalConDescuento=(totalSinDescuento-((totalSinDescuento*80)/100));
             totalPagar.innerText =totalConDescuento;
         }
-        if(categoria==2){
+        else if(categoria==2){
             totalConDescuento=(totalSinDescuento-((totalSinDescuento*50)/100));
             totalPagar.innerText =totalConDescuento;
         }
-        if(categoria==3){
+        else if(categoria==3){
             totalConDescuento=(totalSinDescuento-((totalSinDescuento*15)/100));
             totalPagar.innerText =totalConDescuento;
         }
-    }
+   
+   
+    } 
 
 
-    // PROCESO
-
-
-    descuentoTicket(selectCategoria,inputCantidad);
-    // SALIDA(S)  
 }
-function borrar(){
+function borrar(e){
+    e.preventDefault();
     totalPagar.innerText = 0;
 
 }
